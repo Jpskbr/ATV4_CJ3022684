3e4eabe9fbac2e379e7cf7979566d49a5e43cc75
Joao Pedro Eleuterio Evaristo <eleuterio.joao@aluno.ifsp.edu.br>
2024-03-20 11:10:01 -0300

    bom

--- ATV4_CJ3022684/Program.cs
+++ ATV4_CJ3022684/Program.cs
@@ -7,6 +7,7 @@ namespace ATV4_CJ3022684
     {
        static void Main(string[] args) 
        {
+            /*
             float op, n1, n2;
             Console.WriteLine("digite o numero de n1;");
             n1 = float.Parse(Console.ReadLine());
@@ -44,7 +45,91 @@ namespace ATV4_CJ3022684
                     Console.WriteLine("subtração");
                     break;
             }
-      
+      */   /* int dia;
+            Console.WriteLine("digite um numero de 1 a 7");
+            dia = int.Parse(Console.ReadLine());
+            
+            switch (dia)
+
+            {
+                case 1:
+
+                    Console.WriteLine("domingo");
+                    break;
+               
+                case 2:
+
+                    Console.WriteLine("segunda-feira");
+                    break;
+                case 3:
+
+                    Console.WriteLine("terça-feira");
+                    break;
+                case 4:
+
+                    Console.WriteLine("quarta-feira");
+                    break;
+                case 5:
+
+                    Console.WriteLine("quinta-feira");
+                    break;
+                case 6:
+
+                    Console.WriteLine("sexta-feira");
+                    break;
+                case 7:
+
+                    Console.WriteLine("sabado");
+                    break;*/
+
+            /*Console.WriteLine("digite o valor total");
+            int valortotal = int.Parse(Console.ReadLine());
+            Console.WriteLine("1- dinheiro avista, 2- pix avista, 3- debito, 4- credito");
+            int op = int.Parse(Console.ReadLine());
+            
+
+            switch (op)
+            {
+                case 1:
+
+                    float vista = valortotal - ((valortotal / 100) * 15) ;
+                    Console.WriteLine("o valor total é {0}", vista);
+                    break;
+                case 2:
+
+                    float vista2 = valortotal - ((valortotal /100) * 10) ;
+                    Console.WriteLine("o valor é {0}", vista2);
+                    break;
+                case 3:
+
+                    float taxa = valortotal + ((valortotal / (100)) * 5) ;
+                    Console.WriteLine("o valor total é {0}", taxa);
+                    break;
+                case 4:
+
+                    float taxa1 = valortotal + ((valortotal / (100)) * 10);
+                    Console.WriteLine("o valor é{0}", taxa1);
+                    break;*/
+
+            int mes;
+            Console.WriteLine("digite um numero de 1 a 12");
+            Console.WriteLine("1- janeiro, 2- fevereiro, 3- março, 4- abril, 5- maio, ");
+            int op = int.Parse(Console.ReadLine());
+
+
+            
+            }
+
+
         } 
+
     }
 }
+
+
+
+ 
+
+
+
+
